be20ccbdeb6cc07bb4e0292fa78bd6e0ff03d82e
Vu Hoang <20161728@student.hust.edu.vn>
2020-05-30 06:16:28 +0700

    insertion sort

--- components/index.ts
+++ components/index.ts
@@ -13,10 +13,7 @@ export { default as AutoTransformGroup } from './AutoTransformGroup';
 export { default as Array } from './Array';
 export { default as Queue } from './Queue';
 export { default as Stack } from './Stack';
-<<<<<<< HEAD
 export { default as Line } from './Line';
-=======
 export { default as HTMLRenderer } from './HTMLRenderer';
 export { default as CustomDropDown } from './CustomDropDown';
 export { default as ParameterInputPopover } from './ParameterInputPopover';
->>>>>>> 2f70c705f219970d7196da8ff22aa29c27a1e327
